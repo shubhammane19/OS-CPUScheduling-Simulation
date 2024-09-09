6e80735d14ff0f675e309a3ffcfd0c09e6c3f8c6
Skncoe -Shubham Mane <shubhammanepm@gmail.com>
2024-09-09 20:01:10 +0530

    First Commit

---
+++ CPU-Simulation/src/main/java/com/CPU_Scheduling/CPU_Simulation/CpuSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.CPU_Scheduling.CPU_Simulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpuSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpuSimulationApplication.class, args);
+	}
+
+}
---
+++ CPU-Simulation/src/test/java/com/CPU_Scheduling/CPU_Simulation/CpuSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CPU_Scheduling.CPU_Simulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpuSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
